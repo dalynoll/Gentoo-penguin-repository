367dfe43c5b0b812454a2d82426d7a8d3a812d5b
dalynoll <dalynoll@gmail.com>
2025-02-10 17:01:56 -0300

    Update selective_sweep.sh

--- Adaptive divergence paper/selective_sweep.sh
+++ Adaptive divergence paper/selective_sweep.sh
@@ -1,1 +1,29 @@
+list=$(cat "scaff_name.txt")
 
+for i in $list;
+do vcftools  --vcf  $VCF_FILE  --chr $i  --recode --recode-INFO-all --out  $i&
+done
+wait```
+
+
+
+###### Running RAiSD
+
+```bash
+list=$(cat "$vcf/scaff_name.txt")
+
+for i in $list
+do
+RAiSD -n ${i} -I $vcf/${i}.vcf -D -P -O -A 0.99 -s -t -R 
+done
+
+
+
+###### Running nSL
+```bash
+list=$(cat "$vcf/scaff_name.txt")
+
+for i in $list
+do
+./selscan --nsl --unphased --ehh-win 10000 --vcf ${i}.vcf --out ${i}
+done
