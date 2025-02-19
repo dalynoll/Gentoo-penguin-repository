eda11f5f65283da652452c9e991a860010de9705
dalynoll <dalynoll@gmail.com>
2025-02-19 12:16:42 -0300

    Update Species_Trees.sh

--- Adaptive divergence paper/Species_Trees.sh
+++ Adaptive divergence paper/Species_Trees.sh
@@ -136,12 +136,29 @@ xargs -P 30 -I {} bash -c '
 ######################## Gene trees ########################
 ############################################################
 
+source activate iqtree
+mkdir -p 5_gene_trees_iqtree
+
+#!/bin/bash
+
+mkdir -p 5_gene_trees_iqtree
+
+ls 4_align_mafft_2269/*.fa | xargs -P 4 -I {} bash -c '
+  f="{}"
+  base=$(basename "$f" .fa)
+  iqtree2 -s "$f" -m MFP -bb 1000 -T 8 -pre "5_gene_trees_iqtree/$base"
+'
 
 
+###############################################################
+######################## Species trees ########################
+###############################################################
 
+mkdir -p 6_SpeciesTree_astral
 
-
-
+java -jar /data2/daly/gentoo/filogenias/cds/programas/ASTRAL/Astral/astral.5.7.8.jar \
+  -i lista_trees_gene.txt \
+  -o species_tree.tre
 
 
 
