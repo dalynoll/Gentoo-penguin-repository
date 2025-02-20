f439ca6792a8c2fb0b56c12d4231302c12a50555
dalynoll <dalynoll@gmail.com>
2025-02-20 10:44:50 -0300

    Update 3_seq_alignment.sh

--- Adaptive divergence paper/phylogenomics/3_seq_alignment.sh
+++ Adaptive divergence paper/phylogenomics/3_seq_alignment.sh
@@ -1,1 +1,16 @@
+############################################################################
+######################## Align sequences with MAFFT ########################
+############################################################################
+
+#!/bin/bash
+source activate mafft
+mkdir -p 4_align_mafft
+
+ls 3_seq_genes/*.fa | \
+xargs -P 30 -I {} bash -c '
+  f="{}"
+  b=$(basename "$f" .fa)
+  echo "Alineando $f ..."
+  mafft --auto "$f" > "4_align_mafft/${b}_aligned.fa"
+'
 
