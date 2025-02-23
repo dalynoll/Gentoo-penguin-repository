f4dadb6171fc80a3fdc66c9ab346426ed8f2bec9
dalynoll <dalynoll@gmail.com>
2025-02-22 23:00:27 -0300

    Update 4_species_tree.sh

--- Adaptive divergence paper/phylogenomics/4_species_tree.sh
+++ Adaptive divergence paper/phylogenomics/4_species_tree.sh
@@ -1,1 +1,15 @@
+#####################################################
+################## Preliminar tree ##################
+#####################################################
 
+## Use catfasta2phyml.pl (https://github.com/nylander/catfasta2phyml) to concatenate all CDS
+perl catfasta2phyml.pl *.fa --intersect > cds_10102_gentoo.fa 2> partitions.txt
+
+## Generate a preliminar tree using IQ-Tree
+
+#!/bin/bash
+source activate iqtree
+
+iqtree -s cds_10294_gentoo.phy -bb 1000 -T 20 -o adel,chins -m TEST
+
+###### PRELIMINAR TREE OUTPUT:
