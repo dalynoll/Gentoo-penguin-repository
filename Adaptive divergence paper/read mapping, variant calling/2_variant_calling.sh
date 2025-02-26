f0a634afdbb1f738587d38dd7d07cd7507895039
dalynoll <dalynoll@gmail.com>
2025-02-26 16:36:30 -0300

    Create 2_variant_calling.sh

---
+++ Adaptive divergence paper/read mapping, variant calling/2_variant_calling.sh
@@ -0,0 +1,7 @@
+######################################
+########### Variant calling ##########
+######################################
+
+cat chromosome.list.selection | xargs -I {} -n 1 -P 40 sh -c 'bcftools mpileup -Ou \
+-f GCA_010090195.1_BGI_Ppap.V1_genomic.fna -b bam_list -d 5000 -q 10 -Q 20 -a SP,DP \
+--skip-indels -r {} --rf 2 | bcftools call -f GQ -vm -Oz -o {}.vcf.gz'
