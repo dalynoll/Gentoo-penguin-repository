d7620f4510934374f0f942a38d03c79f5a732cee
dalynoll <dalynoll@gmail.com>
2025-02-26 10:36:48 -0300

    Update 1_pre_processing_gff.sh

--- Adaptive divergence paper/phylogenomics/1_pre_processing_gff.sh
+++ Adaptive divergence paper/phylogenomics/1_pre_processing_gff.sh
@@ -4,3 +4,7 @@
 cat GCA_010090195.1_BGI_Ppap.V1_genomic.gff |grep -E '(gene|mRNA|exon|CDS)' > GCA_010090195.1_BGI_Ppap.V1_genomic_filtered.gff
 cat GCA_030674165.1_ASM3067416v1_genomic.gff |grep -E '(gene|mRNA|exon|CDS)' > GCA_030674165.1_ASM3067416v1_genomic_filtered.gff
 
+
+# get longest isoform
+agat_sp_keep_longest_isoform.pl -g gentoo_genomic_filtered.gff -o gentoo_genomic_filtered_longest.gff 
+agat_sp_keep_longest_isoform.pl -g southgeorgias_genomic_filtered.gff -o southgeorgias_genomic_filtered_longest.gff 
