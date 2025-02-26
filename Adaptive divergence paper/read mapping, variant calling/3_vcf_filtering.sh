82dc7c54e57a2788d3c7beda0f400d4be69fb6fe
dalynoll <dalynoll@gmail.com>
2025-02-26 16:42:34 -0300

    Update 3_vcf_filtering.sh

--- Adaptive divergence paper/read mapping, variant calling/3_vcf_filtering.sh
+++ Adaptive divergence paper/read mapping, variant calling/3_vcf_filtering.sh
@@ -1,1 +1,13 @@
+  # # # # # ## # # # # ## # # # # # # # # #
+# # # # # # Variant Normalization # # # # # #
+  # # # # # ## # # # # ## # # # # # # # # #
+
+bcftools norm --check-ref w -f GCA_010090195.1_BGI_Ppap.V1_genomic.fna \
+-o unfiltered_gentoo_norm.vcf.gz -Oz --threads 8  unfiltered_gentoo.vcf
+
+
+  # # # # # # # # # # ## # # # # # # # # # # # # #
+# # # # # # Filtering of SNPs and Indels # # # # # #
+  # # # # # # # # # # ## # # # # # # # # # # # # #
+bcftools filter --SnpGap 5 --IndelGap 5 -Oz -o gentoo_indels.vcf.gz  unfiltered_gentoo_norm.vcf
 
